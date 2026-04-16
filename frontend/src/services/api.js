2526877c0d3e30e0611393cf5d643b27691ccbe1
Saptarsi-RITS <Saptarsi.Das@revappayyaitservices.com>
2026-04-16 22:36:55 +0530

    render url added

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:5000";
+const BASE_URL = "https://quantyra-assignment-saptarsi.onrender.com";
 
 export const submitFeedback = async (text) => {
   const res = await fetch(`${BASE_URL}/api/feedback`, {
